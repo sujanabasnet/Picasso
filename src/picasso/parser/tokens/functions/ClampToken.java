3213ac9c6921f8651516aa8a3e6c80d569951519
slmartin281 <martins23@mail.wlu.edu>
2021-11-30 13:39:06 -0500

    Abs and Clamp Tokens

---
+++ src/picasso/parser/tokens/functions/ClampToken.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the ceiling function token.
+ * 
+ * @author Sarah Martin
+ */
+public class ClampToken extends FunctionToken {
+
+	public ClampToken() {
+		super("Clamp Function Token");
+	}
+
+}
