a4e2a9502f294f2a9b0eb3d0c2101e45633040ba
sujanabasnet <basnets23@mail.wlu.edu>
2021-11-30 17:18:18 -0500

    Fixed the error.

--- src/tests/TokenizerTest.java
+++ src/tests/TokenizerTest.java
@@ -87,6 +87,13 @@ public class TokenizerTest {
 		assertEquals(new LeftParenToken(), tokens.get(1));
 		assertEquals(new IdentifierToken("x"), tokens.get(2));
 		assertEquals(new RightParenToken(), tokens.get(3));
+		
+		expression = "ceil(y)";
+		tokens = tokenizer.parseTokens(expression);
+		assertEquals(new CeilToken(), tokens.get(0));
+		assertEquals(new LeftParenToken(), tokens.get(1));
+		assertEquals(new IdentifierToken(""), tokens.get(2));
+		assertEquals(new RightParenToken(), tokens.get(3));
 	}
 
 	@Test
