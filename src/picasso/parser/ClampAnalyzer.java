4a26c7568ac2ea50af2a67924ca08ea7e7dd786c
slmartin281 <martins23@mail.wlu.edu>
2021-11-30 13:31:24 -0500

    Create Clamp and its Analyzer

---
+++ src/picasso/parser/ClampAnalyzer.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Clamp;
+import picasso.parser.tokens.Token;
+
+/**
+ * @author sarahmartin
+ *
+ */
+public class ClampAnalyzer extends UnaryFunctionAnalyzer {
+
+	/**
+	 * 
+	 */
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); 
+		return new Clamp(SemanticAnalyzer.getInstance().generateExpressionTree(tokens));
+	}
+
+}
