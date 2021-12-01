7a1460c224850228dbe6cba9ec7ae0a25ee38c38
Jared Cordova <cordovaj22@mail.wlu.edu>
2021-12-01 00:06:41 -0500

    Created the Cosine Token

---
+++ src/picasso/parser/tokens/functions/CosineToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Cosine function token
+ * 
+ */
+public class CosineToken extends FunctionToken {
+
+	public CosineToken() {
+		super("Cosine Function Token");
+	}
+
+}
