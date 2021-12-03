27adde86980a53ef05be3833475e077dc7058f31
minimellow10 <marsha22@mail.wlu.edu>
2021-12-03 10:55:04 -0500

    fixed basic function expression tokenizer test.

--- src/tests/TokenizerTest.java
+++ src/tests/TokenizerTest.java
@@ -92,7 +92,7 @@ public class TokenizerTest {
 		tokens = tokenizer.parseTokens(expression);
 		assertEquals(new CeilToken(), tokens.get(0));
 		assertEquals(new LeftParenToken(), tokens.get(1));
-		assertEquals(new IdentifierToken(""), tokens.get(2));
+		assertEquals(new IdentifierToken("y"), tokens.get(2));
 		assertEquals(new RightParenToken(), tokens.get(3));
 	}
 
