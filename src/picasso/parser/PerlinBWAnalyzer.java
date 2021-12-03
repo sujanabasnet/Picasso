f82f7f030eb7ab91388aba965ede45e97cb70083
minimellow10 <marsha22@mail.wlu.edu>
2021-12-03 12:26:10 -0500

    Added PerlinBW class and functionality, also added tests for PerlinBW and PerlinColor.

---
+++ src/picasso/parser/PerlinBWAnalyzer.java
@@ -0,0 +1,26 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.PerlinBW;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the plus or "addition function".
+ * 
+ 
+ */
+public class PerlinBWAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the plus token
+		// the parameters are the next tokens on the stack.
+		// But, they need to be processed
+		ExpressionTreeNode expr2 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		ExpressionTreeNode expr1 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		return new  PerlinBW(expr1, expr2);
+	}
+
+}
---
+++ src/picasso/parser/language/expressions/PerlinBW.java
@@ -0,0 +1,21 @@
+package picasso.parser.language.expressions;
+
+import picasso.model.ImprovedNoise;
+import picasso.parser.language.ExpressionTreeNode;
+
+public class PerlinBW extends MultipleArgumentFunctions {
+
+	public PerlinBW(ExpressionTreeNode expr1, ExpressionTreeNode expr2) {
+		super(expr1, expr2);
+	}
+
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor a = expr1.evaluate(x, y);
+		RGBColor b = expr2.evaluate(x, y);
+		double grey = ImprovedNoise.noise(a.getRed() + b.getRed(), a.getGreen() + b.getGreen(),
+				a.getBlue() + b.getBlue());
+		return new RGBColor(grey, grey, grey);
+	}
+
+}
