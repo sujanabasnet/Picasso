ded1210e2da8d6846d787610be6ce9789c92ec1b
slmartin281 <martins23@mail.wlu.edu>
2021-12-05 22:37:15 -0500

    added random(), but not working. Constructor args issue

---
+++ src/picasso/parser/RandomAnalyzer.java
@@ -0,0 +1,27 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.PerlinColor;
+import picasso.parser.language.expressions.Randomizer;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the plus or "addition function".
+ * @author sarahmartin
+ 
+ */
+public class RandomAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the plus token
+		// the parameters are the next tokens on the stack.
+		// But, they need to be processed
+		//ExpressionTreeNode expr2 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		//ExpressionTreeNode expr1 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		return new Randomizer();
+	}
+
+}
