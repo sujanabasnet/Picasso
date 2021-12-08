abf2b636b059e866beea92ba3d0388b1056484eb
Praise <ipadpraise@gmail.com>
2021-12-08 17:56:30 -0500

    Added Modulo Binary Operator

---
+++ src/picasso/parser/tokens/operations/ModToken.java
@@ -0,0 +1,15 @@
+package picasso.parser.tokens.operations;
+
+import picasso.parser.language.CharConstants;
+import picasso.parser.tokens.chars.CharToken;
+
+/**
+ * Represents the modulo sign token
+ * 
+ * @author Praise Apata
+ */
+public class ModToken extends CharToken implements OperationInterface {
+	public ModToken() {
+		super(CharConstants.MOD);
+	}
+}
