3f132f0e16c2ca1dff84ab5a9681b94118af2301
minimellow10 <marsha22@mail.wlu.edu>
2021-12-08 13:44:08 -0500

    Created Assignment operation.

---
+++ src/picasso/parser/EqualsAnalyzer.java
@@ -0,0 +1,31 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Equals;
+import picasso.parser.tokens.IdentifierToken;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the plus or "addition function".
+ * 
+ * @author andrew marsh
+ * 
+ */
+public class EqualsAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); 
+		ExpressionTreeNode expr = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		if (tokens.peek() instanceof IdentifierToken) {
+			IdentifierToken t = (IdentifierToken) tokens.pop();
+			String id = t.getName();
+			return new Equals(id, expr);
+		}
+		throw new ParseException("Expected a variable name.");
+		
+	}
+
+}
