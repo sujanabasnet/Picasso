1e0f744e31065bdfe58e3ee33b7257a342e3bbd7
slmartin281 <martins23@mail.wlu.edu>
2021-12-09 12:36:19 -0500

    added mod binop, tests

--- src/picasso/parser/ModAnalyzer.java
+++ src/picasso/parser/ModAnalyzer.java
@@ -3,24 +3,24 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Multiplication;
+import picasso.parser.language.expressions.Mod;
 import picasso.parser.tokens.Token;
 
 /**
- * Handles parsing the star or "multiplication function".
+ * Handles parsing of the mod function.
  * 
  * @author Robert C. Duvall
  * @author Sarah Martin
  * 
  */
-public class StarAnalyzer implements SemanticAnalyzerInterface {
+public class ModAnalyzer implements SemanticAnalyzerInterface {
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Remove the star token
+		tokens.pop(); // Remove the mod token
 		ExpressionTreeNode second = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
 		ExpressionTreeNode first = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		return new Multiplication(first, second);
+		return new Mod(first, second);
 	}
 
 }
