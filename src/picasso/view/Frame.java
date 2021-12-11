7293d74e92bd83c372de09a0488f608e92eb882f
Jared Cordova <cordovaj22@mail.wlu.edu>
2021-12-11 15:46:48 -0500

    Rename our "Save" button to save the image

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -44,7 +44,7 @@ public class Frame extends JFrame {
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
-		commands.add("Save", new Writer()); 
+		commands.add("Save JPG Image", new Writer()); 
 		
 		// add input window and evaluate button 
 		JPanel inputPane = new JPanel();
