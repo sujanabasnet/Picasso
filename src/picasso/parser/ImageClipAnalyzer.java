02e6b2f6779c96b496fe08e972b1f38b5442c866
sujanabasnet <basnets23@mail.wlu.edu>
2021-12-13 21:31:55 -0500

    Added ImgaeClip.

---
+++ src/picasso/parser/ImageClipAnalyzer.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.ImageClip;
+import picasso.parser.tokens.StringToken;
+import picasso.parser.tokens.Token;
+
+/**
+ * @author sujanabasnet
+ *
+ */
+public class ImageClipAnalyzer implements SemanticAnalyzerInterface {
+
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop();
+		
+		ExpressionTreeNode expr2 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		ExpressionTreeNode expr1 = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+		
+		StringToken token = (StringToken) tokens.pop();
+		String fileName = token.getName();
+		
+		return new ImageClip(fileName, expr1, expr2);
+
+	}
+
+}
