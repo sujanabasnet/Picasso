0546bf895b4482c911194ddf1160b8e220785b32
minimellow10 <marsha22@mail.wlu.edu>
2021-12-14 10:53:07 -0500

    Uppercase letters expressions

--- src/picasso/parser/StringAnalyzer.java
+++ src/picasso/parser/StringAnalyzer.java
@@ -32,6 +32,7 @@ public class StringAnalyzer implements SemanticAnalyzerInterface {
 		idToExpression.put("y", new Y());
 		
 		//all characters
+		//lowercase letter
 		charToExpression.put("a", new Sin(new X()));
 		charToExpression.put("b", new Cos(new Y()));
 		charToExpression.put("c", new Tan(new X()));
@@ -58,7 +59,33 @@ public class StringAnalyzer implements SemanticAnalyzerInterface {
 		charToExpression.put("x", new Mod(new Cos(new X()), new Y()));
 		charToExpression.put("y", new PerlinColor(new Division(new X(), new Y()), new X()));
 		charToExpression.put("z", new Abs(new Multiplication(new Y(), new X())));
-		
+		//uppercase letters
+		charToExpression.put("A", new Sin(new Y()));
+		charToExpression.put("B", new Cos(new X()));
+		charToExpression.put("C", new Tan(new X()));
+		charToExpression.put("D", new Atan(new X()));
+		charToExpression.put("E", new Abs(new Subtraction(new X(), new Y())));
+		charToExpression.put("F", new Ceil(new Exponentiate(new Y(), new X())));
+		charToExpression.put("G", new Floor(new Addition(new Y(), new X())));
+		charToExpression.put("H", new Log(new Mod(new Y(), new X())));
+		charToExpression.put("I", new PerlinBW(new Addition(new X(), new Y()), new Y()));
+		charToExpression.put("J", new Ycrcbtorgb(new Exponentiate(new Y(), new X())));
+		charToExpression.put("K", new Rgbtoycrcb(new Mod(new Y(), new X())));
+		charToExpression.put("L", new Exp(new Division(new X(), new Y())));
+		charToExpression.put("M", new Cos(new PerlinColor(new X(), new Y())));
+		charToExpression.put("N", new Atan(new Subtraction(new Floor(new Y()), new X())));
+		charToExpression.put("O", new Addition(new Subtraction(new Y(), new X()), new PerlinColor(new X(), new Ceil(new Y()))));
+		charToExpression.put("P", new Multiplication(new Exponentiate(new X(), new Y()), new Ycrcbtorgb( new X())));
+		charToExpression.put("Q", new Division(new Addition(new X(), new Y()), new Y()));
+		charToExpression.put("R", new Exp(new Mod(new Y(), new Ycrcbtorgb(new X()))));
+		charToExpression.put("S", new Rgbtoycrcb(new Multiplication(new X(), new Floor(new Y()))));
+		charToExpression.put("T", new Exp(new Division(new Y(), new X())));
+		charToExpression.put("U", new Division(new Mod(new Y(), new X()), new Atan(new Y())));
+		charToExpression.put("V", new Cos(new Rgbtoycrcb(new Y())));
+		charToExpression.put("W", new Multiplication(new Subtraction(new X(), new Y()), new Y()));
+		charToExpression.put("X", new Exponentiate(new Sin(new X()), new Y()));
+		charToExpression.put("Y", new PerlinColor(new Multiplication(new X(), new Y()), new Y()));
+		charToExpression.put("Z", new Log(new Division(new Y(), new X())));
 		
 		
 		
